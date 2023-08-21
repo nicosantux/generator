2bb3dc63949558aa8ff504bc19bfe63305f65dbf
Nicolas Santuccio <nico.santuccio@gmail.com>
2023-08-21 15:58:19 -0300

    add generator list options

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,11 +5,22 @@ module.exports = class extends Generator {
   welcome() {
     this.log(`
     ==================================================================
-                    Welcome to Santux-Generator!
-
-    Type yo santux:<option> to start. For example: yo santux:next-ts
-
-    Options: next-ts, react-ts, react, node-ts, vscode
+                      Welcome to Santux-Generator!
     ==================================================================`);
+  }
+
+  async prompting() {
+    this.answer = await this.prompt([
+      {
+        choices: ["next-ts", "node-ts", "react", "react-ts", "vscode"],
+        message: "Which package manager does your project use?",
+        name: "generator",
+        type: "list",
+      },
+    ]);
+  }
+
+  install() {
+    this.spawnCommandSync(`yo santux:${this.answer.generator}`);
   }
 };
